e0ae785ea70efc38e47f11ddc9cc4b417e2e771d
Jaime Bruno Cirne de Oliveira <jaimecirne@gmail.com>
2024-01-02 14:01:09 -0300

    Update tips.sh

--- tips.sh
+++ tips.sh
@@ -3,3 +3,9 @@ find . ! -empty -type f -exec md5sum {} + | sort | uniq -w32 -dD
 
 #when you forget using sudo and don't wanna lose vim editing
 :w !sudo tee % >/dev/null 
+
+# select default java installed
+sudo update-alternatives --config java
+
+#
+du -h --max-depth=1
